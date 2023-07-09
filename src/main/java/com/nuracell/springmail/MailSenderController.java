a4a3533117653a3296295535fd4ffb612bbde022
nurace11 <67009058+nurace11@users.noreply.github.com>
2023-07-09 15:40:59 +0600

    Update MailSenderController.java

--- src/main/java/com/nuracell/springmail/MailSenderController.java
+++ src/main/java/com/nuracell/springmail/MailSenderController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/email")
 @RequiredArgsConstructor
